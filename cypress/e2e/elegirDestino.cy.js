83ce9c5c838b40bc4c8faaae44dd88889bb8442c
narvaezjacome <narvaezjacome@gmail.com>
2023-06-15 08:33:25 -0500

    automatizacion de los campos requeridos realizadas

--- cypress/e2e/elegirDestino.cy.js
+++ cypress/e2e/elegirDestino.cy.js
@@ -15,7 +15,7 @@ describe('elegir destino', () => {
         cy.wait(2000) // Ajusta el tiempo de espera según sea necesario
 
         // Seleccionar el origen y destino de los vuelos
-        cy.get('#inputOrigen').type('Bogot')
+        cy.get('.input-origen').type('Bogot')
         cy.get('#comboOrigen').click()
 
 
@@ -24,14 +24,21 @@ describe('elegir destino', () => {
         cy.wait(2000)
 
 
+        cy.get('#selectPasj').click()
+        cy.get('.bts-add').eq(0).find('.plus').click({force: true})
+        cy.wait(2000)
+
+
         cy.get('#inputOutboundDate').find('.valid').eq(0).click({force: true})
         cy.wait(2000)
         cy.get('.return-selected').find('.month-wrapper').find('.valid').eq(30).click({force: true})
-
-        cy.get('#selectPasj').click()
-        cy.get('#passenger').find('.bts-add').find('.plus').click()
         cy.wait(2000)
 
+
+    /*    cy.get('.btn-search').click()
+        cy.wait(2000)
+
+        cy.get('.search-results').should('be.visible') */
 
   })
 })
