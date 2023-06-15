a4dc73ee9493f1cbd5e14267f9a2eca4c574cc1f
narvaezjacome <narvaezjacome@gmail.com>
2023-06-15 17:56:27 -0500

    automatizaciones completas asi mismo con las validaciones respectivas

--- cypress/e2e/elegirDestino.cy.js
+++ cypress/e2e/elegirDestino.cy.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 describe('elegir destino', () => {
   it('navegar', () => {
 
@@ -15,6 +17,7 @@ describe('elegir destino', () => {
         cy.wait(2000) // Ajusta el tiempo de espera según sea necesario
 
         // Seleccionar el origen y destino de los vuelos
+
         cy.get('.input-origen').type('Bogot')
         cy.get('#comboOrigen').click()
 
@@ -24,21 +27,62 @@ describe('elegir destino', () => {
         cy.wait(2000)
 
 
+
+        // Seleccionar el numero de pasajeros adultos
+
         cy.get('#selectPasj').click()
         cy.get('.bts-add').eq(0).find('.plus').click({force: true})
         cy.wait(2000)
 
 
+
+        // Seleccionar fecha valida de ida
+
         cy.get('#inputOutboundDate').find('.valid').eq(0).click({force: true})
         cy.wait(2000)
+
+
+        // Seleccionar fecha valida de regreso
         cy.get('.return-selected').find('.month-wrapper').find('.valid').eq(30).click({force: true})
         cy.wait(2000)
 
 
-    /*    cy.get('.btn-search').click()
-        cy.wait(2000)
+        // VALIDACION CAMPOS
 
-        cy.get('.search-results').should('be.visible') */
+
+        //Valida campo de origen
+        cy.get('.styledSelectOrigen').should('have.text','Bogotá (BOG) El Dorado')
+
+        //Valida campos de destino
+        cy.get('.styledSelectDestino').should('have.text','Cali (CLO) Alfonso Bonilla Aragón')
+
+        //Valida la cantidad de pasajeros
+        cy.get('#tPasajeros').should('have.text','2 Pasajeros')
+
+
+
+        //Valida fecha de ida y regreso
+
+        cy.get('.info-select-start').as('fechaIda')
+        //cy.get('@fechaIda').invoke('text').then((fechaIda)
+
+        cy.get('.info-select-end').as('fechaRegreso')
+        //cy.get('@fechaRegreso').invoke('text').then((fechaRegreso)
+
+       cy.get('@fechaIda').invoke('val').then((fechaIda) => {
+          cy.get('@fechaRegreso').invoke('val').then((fechaRegreso) => {
+
+            const fechaIdaObj = dayjs(fechaIda);
+            const fechaRegresoObj = dayjs(fechaRegreso);
+
+            const diferenciaMes = fechaRegresoObj.diff(fechaIdaObj, 'months');
+            const esValido = diferenciaMes >= 1;
+
+            expect(esValido).to.be.false;
+          });
+        });
+
+
 
   })
 })
